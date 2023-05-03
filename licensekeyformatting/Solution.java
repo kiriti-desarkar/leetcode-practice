9670729ec6f45a64ce00fffaeeefcf5cd10163be
kiriti-desarkar <kiriti.desarkar.mails@gmail.com>
2023-05-03 20:01:55 +0530

    license key formatting

---
+++ licensekeyformatting/Solution.java
@@ -0,0 +1,22 @@
+package licensekeyformatting;
+
+public class Solution {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (c != '-') {
+                sb.append(Character.toUpperCase(c));
+                count++;
+                if (count % k == 0) {
+                    sb.append('-');
+                }
+            }
+        }
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '-') {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        return sb.reverse().toString();
+    }
+}
